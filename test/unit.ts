63024247a62e451f332f8cad8c4dc9d98d2b3829
valaxy <valaxy@qq.com>
2016-02-27 16:17:11 +0800

    rename test file

--- test/unit.ts
+++ test/unit.ts
@@ -2,9 +2,9 @@ require('./case/UseCase')
 require('./case/Executor')
 require('./case/QueueExecute')
 
-require('./rpc/ajax')
+require('./rpc/Ajax')
 
-require('./event/event')
+require('./event/Event')
 require('./event/Observer')
 require('./event/Subscriber')
 
